01d36f5aef9a6d3b58d09727a1c8e81cff7d76e6
Xer0 <tylergrayx@gmail.com>
2018-06-02 00:13:33 -0400

    notes

--- config.js
+++ config.js
@@ -1,5 +1,7 @@
 'use strict';
 
+exports.CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || 'http://localhost:3000';
+
 exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/imp4gif';
 
 
--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
-app.use(express.static('public'));
+// app.use(express.static('public'));
 app.use(morgan('common'));
 
 
