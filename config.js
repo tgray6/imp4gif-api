cab6526b815ca5ed0f2f51ac05a693800e9bbad1
Xer0 <tylergrayx@gmail.com>
2018-06-05 00:30:14 -0400

    changed ports

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 'use strict';
 
-exports.CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || 'http://localhost:3000';
+exports.CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || 'http://localhost:9999';
 
 exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/imp4gif';
 
@@ -8,4 +8,4 @@ exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/imp4gif'
 // exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://tgray6:sagesage1@ds239940.mlab.com:39940/imp4gif';
 
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://tgray6:sagesage1@ds239940.mlab.com:39940/imp4gif-test';
-exports.PORT = process.env.PORT || 8080;
+exports.PORT = process.env.PORT || 8888;
