79a3d15ee6565f0172916f69cf7e550faad4869b
Xer0 <tylergrayx@gmail.com>
2018-06-21 19:14:31 -0400

    changed database and client origin

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 'use strict';
 
-exports.CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || 'http://localhost:9999';
-// exports.CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || 'https://imp4gif.netlify.com/';
+// exports.CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || 'http://localhost:9999';
+exports.CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || 'https://imp4gif.netlify.com/';
 
 
 
-exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/imp4gif';
-// exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://tgray6:sagesage1@ds239940.mlab.com:39940/imp4gif';
+// exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/imp4gif';
+exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://tgray6:sagesage1@ds239940.mlab.com:39940/imp4gif';
 
 
 
