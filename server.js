0575efb88ceffc152eb90f1b1925ab2f3c481f58
Xer0 <tylergrayx@gmail.com>
2018-06-21 22:46:35 -0400

    CORS

--- server.js
+++ server.js
@@ -8,27 +8,27 @@ mongoose.Promise = global.Promise;
 // const PORT = process.env.PORT || 3000;
 
 const cors = require('cors');
-const { CLIENT_ORIGIN } = require('./config');
+// const { CLIENT_ORIGIN } = require('./config');
 const { PORT, DATABASE_URL } = require('./config');
 const { Items } = require('./models');
 
 app.use(cors());
 
-app.use(
-    cors({
-        origin: CLIENT_ORIGIN
-    })
-);
+// app.use(
+//     cors({
+//         origin: CLIENT_ORIGIN
+//     })
+// );
 
-app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
-  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
-  if (req.method === 'OPTIONS') {
-    return res.send(204);
-  }
-  next();
-});
+// app.use(function (req, res, next) {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+//   res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
+//   if (req.method === 'OPTIONS') {
+//     return res.send(204);
+//   }
+//   next();
+// });
 
 
 
